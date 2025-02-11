3f69f1c973206ac9409d522b58fd33f36215af7d
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-02-11 12:04:06 +0100

    :tada: initialize project structure with environment configuration, Docker setup, and basic API framework
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

---
+++ gui/app.py
@@ -0,0 +1,5 @@
+from typing import Optional, Union, Dict
+
+def test() -> Union[bool, Dict[str, list[str]]] | None:
+    pass
+    
---
+++ lidarr/api.py
@@ -0,0 +1,14 @@
+import asyncio
+from aiopyarr.models.host_configuration import PyArrHostConfiguration
+from aiopyarr.lidarr_client import LidarrClient
+
+class Api:
+    def __init__(self):
+        self.client: LidarrClient
+    
+    
+
+
+
+if __name__ == "__main__":
+    pass
